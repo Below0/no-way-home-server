03792376e12e48370d601616e6fdf2c60eacb561
Below0 <mutex@kakao.com>
2022-01-27 19:51:37 +0900

    ADD: root_path env

--- batch/ConfigManager.py
+++ batch/ConfigManager.py
@@ -1,12 +1,17 @@
 import json
+import os
 
 class ConfigManager:
 
     @staticmethod
     def get_config(config_type):
-        with open("./config.json") as json_file:
+        ROOT_PATH = os.getenv("NO_WAY_HOME_PATH")
+        with open(f"{ROOT_PATH}/config.json") as json_file:
             db_config = json.load(json_file)
 
         return db_config.get(config_type, None)
 
         
+if __name__ == "__main__":
+    db_config = ConfigManager.get_config("db")
+    print(db_config)
